0834fec3524b05873fd15f1f4670f08b57b825a7
ccichon <wizkid238@github.com>
2014-03-04 03:48:41 +0000

    update to read game id and name from actual xml

--- public_html/cgi-bin/build_catalog.pl
+++ public_html/cgi-bin/build_catalog.pl
@@ -10,7 +10,7 @@ print header('text/xml');
 my ($prop_file, $properties);
 
 #load hash for Names of Sport
-$prop_file="/home/ccichon/rtss-code/properties/sport.properties";
+$prop_file="/home/realtin5/properties/sport.properties";
 open PROPS, "< $prop_file" or die "unable to open properties $prop_file\n";
 $properties= new Config::Properties();
 $properties->load(*PROPS);
@@ -18,23 +18,33 @@ my %sport_keys=$properties->properties;
 close PROPS;
 
 #load hash for Names of Level
-$prop_file="/home/ccichon/rtss-code/properties/level.properties";
+$prop_file="/home/realtin5/properties/level.properties";
 open PROPS, "< $prop_file" or die "unable to open properties $prop_file\n";
 $properties= new Config::Properties();
 $properties->load(*PROPS);
 my %level_keys=$properties->properties;
 close PROPS;
 
-#load hash for Names of Locations
-$prop_file="/home/ccichon/rtss-code/properties/location.properties";
+
+#load hash for Names of League
+$prop_file="/home/realtin5/properties/league.properties";
 open PROPS, "< $prop_file" or die "unable to open properties $prop_file\n";
 $properties= new Config::Properties();
 $properties->load(*PROPS);
-my %location_keys=$properties->properties;
+my %league_keys=$properties->properties;
+close PROPS;
+
+
+#load hash for Names of Seasons 
+$prop_file="/home/realtin5/properties/season.properties";
+open PROPS, "< $prop_file" or die "unable to open properties $prop_file\n";
+$properties= new Config::Properties();
+$properties->load(*PROPS);
+my %season_keys=$properties->properties;
 close PROPS;
 
 #load hash for Names of Games
-$prop_file="/home/ccichon/rtss-code/properties/game.properties";
+$prop_file="/home/realtin5/properties/game.properties";
 open PROPS, "< $prop_file" or die "unable to open $prop_file\n";
 $properties= new Config::Properties();
 $properties->load(*PROPS);
@@ -44,7 +54,7 @@ close PROPS;
 
 #my $output = new IO::File (">catalog.xml");
 my $output = '';   # if outputing directly in CGI output
-my $base_dir = "/home/ccichon/rtss-code/catalog";
+my $base_dir = "/home/realtin5/catalog";
 my $writer = new XML::Writer (OUTPUT => $output);
 
 $writer->xmlDecl ('UTF-8');
@@ -56,41 +66,59 @@ while (defined (my $dir_sport = readdir $dh_sport)) {
 	next unless -d "$base_dir/$dir_sport";
 	next if $dir_sport =~ /^\.\.?+$/;
 	my $spt_nme = $sport_keys{$dir_sport};
-	$writer->startTag ('SPORT', 'SPT_ID' => "$dir_sport", 'SPT_NME' => "$spt_nme");
+	$writer->startTag ('SPORT', 'SPRT_ID' => "$dir_sport", 'SPRT_NME' => "$spt_nme");
 
 	opendir (my $dh_level, "$base_dir/$dir_sport") || die "Cannot open $dir_sport $!\n";
 	while (defined (my $dir_level = readdir $dh_level)) {
 		next unless -d "$base_dir/$dir_sport/$dir_level";
 		next if $dir_level =~ /^\.\.?+$/;
 		my $lev_name = $level_keys{$dir_level};
-		$writer->startTag ('LEVEL', 'LEV_ID' => "$dir_level", 'LEV_NAME' => "$lev_name");
+		$writer->startTag ('LEVEL', 'SHR_ID' => "$dir_level", 'NME' => "$lev_name");
 
-		opendir (my $dh_loc, "$base_dir/$dir_sport/$dir_level") || die "Cannot open $dir_level $!\n";
-		while (defined (my $dir_loc = readdir $dh_loc)) {
-			next unless -d "$base_dir/$dir_sport/$dir_level/$dir_loc";
-			next if $dir_loc =~ /^\.\.?+/;
-			my $loc_nme = $location_keys{$dir_loc};
-			$writer->startTag ('LOCATION', 'LOC_ID' => "$dir_loc", 'LOC_NAME', "$loc_nme");
+                opendir (my $dh_league, "$base_dir/$dir_sport/$dir_level") || die "Cannot open $dir_level $!\n";
+                while (defined (my $dir_league = readdir $dh_league)) {
+                        next unless -d "$base_dir/$dir_sport/$dir_level/$dir_league";
+                        next if $dir_league =~ /^\.\.?+/;
+                        my $league_nme = $league_keys{$dir_league};
+ 			$writer->startTag ('LEAGUE', 'SHR_ID' => "$dir_league", 'NME', "$league_nme");
 
-			opendir (my $dh_gmd, "$base_dir/$dir_sport/$dir_level/$dir_loc") || die "Cannot open $dir_loc $!\n";
-			while (defined (my $dir_gmd = readdir $dh_gmd)) {
-				next unless -d "$base_dir/$dir_sport/$dir_level/$dir_loc/$dir_gmd";
-				next if $dir_gmd =~ /^\.\.?+/;
-				my $gmd_name = $games{$dir_gmd};
-				$writer->startTag ('GAME', 'GMD_ID' => "$dir_gmd", 'GMD_NAME' => "$gmd_name");
+			opendir (my $dh_sesn, "$base_dir/$dir_sport/$dir_level/$dir_league") || die "Cannot open $dir_league $!\n";
+			while (defined (my $dir_sesn = readdir $dh_sesn)) {
+				next unless -d "$base_dir/$dir_sport/$dir_level/$dir_league/$dir_sesn";
+				next if $dir_sesn =~ /^\.\.?+/;
+				my $sesn_nme = $season_keys{$dir_sesn};
+				$writer->startTag ('SEASON', 'SHR_ID' => "$dir_sesn", 'NME', "$sesn_nme");
 
-				opendir (my $dh_files, "$base_dir/$dir_sport/$dir_level/$dir_loc/$dir_gmd") || die " Cannot open $dir_gmd $!\n";
-				while (defined (my $file = readdir $dh_files)) {
-					next if $file =~ /^\.\.?+/;
-					next if $file eq "$dir_gmd.xml";
-					if (-f $file) {
-						$file =~ s/.xml$//;
-						#$writer->emptyTag ('STAT_FILE', 'SF' => "$file");
-					}
-				}
-				$writer->endTag ('GAME');
+				opendir (my $dh_gmd, "$base_dir/$dir_sport/$dir_level/$dir_league/$dir_sesn") || die "Cannot open $dir_sesn$!\n";
+				while (defined (my $game = readdir $dh_gmd)) {
+					next unless $game =~ /xml$/;
+					#next if $game =~ /_SF_/;
+					open my $fh, "<", "$base_dir/$dir_sport/$dir_level/$dir_league/$dir_sesn/$game" or die "Couldn't read '$game': $!\n";
+					my @line = grep { /GAME CLT_ID/ } <$fh>;
+					my ($gar1, $clt_id, $gar2, $gar3, $gar4, $gar5, $gar6, $gar7, $gar8, $nme, $gar10) = split /\"/, $line[0];
+					$writer->startTag ('GAME', 'SHR_ID' => "$clt_id", 'NME' => "$nme");
+					close $fh;
+
+					#next unless -d "$base_dir/$dir_sport/$dir_level/$dir_league/$dir_sesn/$dir_gmd";
+					#next if $dir_gmd =~ /^\.\.?+/;
+					#my $gmd_name = $games{$dir_gmd};
+					#$writer->startTag ('GAME', 'SHR_ID' => "$dir_gmd", 'NME' => "$gmd_name");
+
+					#opendir (my $dh_files, "$base_dir/$dir_sport/$dir_level/$dir_league/$dir_sesn/$dir_gmd") || die " Cannot open $dir_gmd $!\n";
+					#while (defined (my $file = readdir $dh_files)) {
+					#	next if $file =~ /^\.\.?+/;
+					#	next if $file eq "$dir_gmd.xml";
+					#	if (-f $file) {
+					#		$file =~ s/.xml$//;
+					#		$writer->emptyTag ('STAT_FILE', 'SF' => "$file");
+					#	}
+					#}
+					$writer->endTag ('GAME');
+ 				}
+                	        $writer->endTag('SEASON');
+
 			}
-			$writer->endTag('LOCATION');
+			$writer->endTag('LEAGUE');
 		}	
 		$writer->endTag('LEVEL');
 	}
